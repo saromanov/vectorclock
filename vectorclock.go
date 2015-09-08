9b1fdab17dc355b3b8d54e7676b28e5a6bb548ae
Sergey Romanov <xxsmotur@gmail.com>
2015-09-08 22:43:17 +0500

    Add comments

--- vectorclock.go
+++ vectorclock.go
@@ -4,12 +4,14 @@ type VectorClock struct {
 	nodes []string
 }
 
+//New provides creating a the new object of VectorClock
 func New()*VectorClock {
 	vc := new(VectorClock)
 	vc.nodes = []string{}
 	return vc
 }
 
+//Add provides append new item for clocks
 func (vc *VectorClock) Add(title string) {
 	vc.nodes = append(vc.nodes, title)
 }
