ea56630440b53d496a8fc975d53e0911973bf606
Sergey Romanov <xxsmotur@gmail.com>
2015-09-08 23:45:21 +0500

    Add InitClocks

--- vectorclock.go
+++ vectorclock.go
@@ -1,17 +1,38 @@
 package vectorclock
 
+type Clock struct {
+	nodes map[string]int
+}
+
 type VectorClock struct {
 	nodes []string
+	clocks map[string] *Clock
 }
 
 //New provides creating a the new object of VectorClock
 func New()*VectorClock {
 	vc := new(VectorClock)
 	vc.nodes = []string{}
+	vc.clocks = map[string]*Clock{}
 	return vc
 }
 
 //Add provides append new item for clocks
 func (vc *VectorClock) Add(title string) {
 	vc.nodes = append(vc.nodes, title)
+}
+
+//InitClocks provides basic initialization for clocks
+func (vc *VectorClock) InitClocks() {
+	for _, item := range vc.nodes {
+		nodes := map[string]int{}
+		for _, value := range vc.nodes {
+			nodes[value] = 0
+		}
+		vc.clocks[item] = &Clock{nodes}
+	}
+}
+
+func (vc *VectorClock) Send(title string) {
+
 }
