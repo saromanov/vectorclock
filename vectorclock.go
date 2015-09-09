f7c598d2f6960b685566981810f4e7ff1cd52104
Sergey Romanov <xxsmotur@gmail.com>
2015-09-09 23:11:40 +0500

    Add Fit

--- vectorclock.go
+++ vectorclock.go
@@ -1,23 +1,17 @@
 package vectorclock
 
-import
-(
+import (
 	"errors"
 	"fmt"
 )
 
-
-type Clock struct {
-	nodes map[string]int
-}
-
 type VectorClock struct {
-	nodes []string
-	clocks map[string] *Clock
+	nodes  []string
+	clocks map[string]*Clock
 }
 
 //New provides creating a the new object of VectorClock
-func New()*VectorClock {
+func New() *VectorClock {
 	vc := new(VectorClock)
 	vc.nodes = []string{}
 	vc.clocks = map[string]*Clock{}
@@ -40,9 +34,17 @@ func (vc *VectorClock) InitClocks() {
 	}
 }
 
+//Fir provides add new event
+func (vc *VectorClock) Fit(title string) error {
+	clock1, ok := vc.clocks[title]
+	if !ok {
+		return errors.New(fmt.Sprintf("%s is not registred", title1))
+	}
+	vc.clocks[title].Set(title)
+}
 
 //Send provides cause between node1 and node2
-func (vc *VectorClock) Send(title1, title2 string)error {
+func (vc *VectorClock) Send(title1, title2 string) error {
 	clock1, ok := vc.clocks[title1]
 	if !ok {
 		return errors.New(fmt.Sprintf("%s is not registred", title1))
@@ -52,5 +54,5 @@ func (vc *VectorClock) Send(title1, title2 string)error {
 	if !ok2 {
 		return errors.New(fmt.Sprintf("%s is not registred", title2))
 	}
-	return nil 
+	return nil
 }
